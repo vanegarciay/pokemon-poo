dcd3659112bee36d294cde9dea2dd67c52925db4
VaneGarciaYara <vanegarciay@gmail.com>
2017-05-07 14:25:07 -0300

    Creacion de 10 pokemon para iniciar con el siguiente ejercicio

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,14 @@ function Pokemon(nombre,color, poderDeAtaque){
 
 const Pikachu = new Pokemon("Pikachu", "Amarillo", 100);
 const Charmander = new Pokemon("Charmander","Rojo",20);
+const Squirtle = new Pokemon("Squirtle", "Azul", 50);
+const Bulbasaur = new Pokemon("Bulbasaur","Verde",55);
+const Butterfly = new Pokemon("Butterfly", "Lila", 25);
+const Mew = new Pokemon("Mew", "Blanco", 100);
+const Jiglypuff = new Pokemon("Jiglypuff", "Rosa", 30);
+const Staryu = new Pokemon("Staryu", "Cafe", 80);
+const Oddish = new Pokemon("Oddish", "Uva", 40);
+const Onix = new Pokemon("Onix", "Gris", 90);
 
 Pikachu.atacar(Charmander);
 
