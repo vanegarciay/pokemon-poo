a680e3ba99f0bd9651c5f4cc00cd3ea1deb9476e
VaneGarciaYara <vanegarciay@gmail.com>
2017-05-07 16:06:53 -0300

    re-factorizacion del Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,27 @@
+main();
+
+function main() {
+    const Pikachu = new Pokemon("Pikachu", "Amarillo", 100);
+    const Charmander = new Pokemon("Charmander","Rojo",20);
+    const Squirtle = new Pokemon("Squirtle", "Azul", 50);
+    const Bulbasaur = new Pokemon("Bulbasaur","Verde",55);
+    const Butterfly = new Pokemon("Butterfly", "Lila", 25);
+    const Mew = new Pokemon("Mew", "Blanco", 100);
+    const Jiglypuff = new Pokemon("Jiglypuff", "Rosa", 30);
+    const Staryu = new Pokemon("Staryu", "Cafe", 80);
+    const Oddish = new Pokemon("Oddish", "Uva", 40);
+    const Onix = new Pokemon("Onix", "Gris", 90);
+
+    var pokemones = [Pikachu,Charmander,Squirtle,Bulbasaur,Butterfly,Mew,Jiglypuff,Staryu,Oddish,Onix];
+
+    rellenarSelectConPokemones("pokemon1", pokemones);
+    rellenarSelectConPokemones("pokemon2", pokemones);
+}
+
 function Pokemon(nombre,color, poderDeAtaque){
     this.nombre = nombre;
     this.color = color;
-
     this.nivelDeAmistad = 0;
-
     this.vida = 100;
     this.poderDeAtaque = poderDeAtaque;
 
@@ -20,39 +38,16 @@ function Pokemon(nombre,color, poderDeAtaque){
     }
 }
 
+function rellenarSelectConPokemones(id, pokemones) {
+    var selectPokemon = document.getElementById(id);
 
-const Pikachu = new Pokemon("Pikachu", "Amarillo", 100);
-const Charmander = new Pokemon("Charmander","Rojo",20);
-const Squirtle = new Pokemon("Squirtle", "Azul", 50);
-const Bulbasaur = new Pokemon("Bulbasaur","Verde",55);
-const Butterfly = new Pokemon("Butterfly", "Lila", 25);
-const Mew = new Pokemon("Mew", "Blanco", 100);
-const Jiglypuff = new Pokemon("Jiglypuff", "Rosa", 30);
-const Staryu = new Pokemon("Staryu", "Cafe", 80);
-const Oddish = new Pokemon("Oddish", "Uva", 40);
-const Onix = new Pokemon("Onix", "Gris", 90);
-
-var pokemones = [Pikachu,Charmander,Squirtle,Bulbasaur,Butterfly,Mew,Jiglypuff,Staryu,Oddish,Onix];
-var selectPokemon1 = document.getElementById("pokemon1");
-var selectPokemon2 = document.getElementById("pokemon2");
-
-
-for(i=0; i<pokemones.length; i++){
-    var option = document.createElement("option");
-    option.text = pokemones[i].nombre;
-    option.value = pokemones[i].nombre;
-    selectPokemon1.add(option);
+    for(i=0; i<pokemones.length; i++){
+        var option = document.createElement("option");
+        option.text = pokemones[i].nombre;
+        option.value = pokemones[i].nombre;
+        selectPokemon.add(option);
+    }
 }
-
-for(i=0; i<pokemones.length; i++){
-    var option = document.createElement("option");
-    option.text = pokemones[i].nombre;
-    option.value = pokemones[i].nombre;
-    selectPokemon2.add(option);
-    
-}
-
-
 
 //Pikachu.atacar(Charmander);
 
