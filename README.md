c8458c8c7aa0f74be659821d749474b67f48cc8d
VaneGarciaYara <vanegarciay@gmail.com>
2017-05-07 20:58:50 -0300

    Adicion de funcionalidades, creacion de entrenadores, pelea bidirecional, estilos, readme, imagenes, header,footer

---
+++ README.md
@@ -0,0 +1,12 @@
+#Pokémon!
+
+* Existen dos entrenadores: Ash y Brock.
+* Las peleas pueden ser en ambas direcciones.
+* Un pokemon sin vida/energía no puede pelear (o seguir peleando) un batalla.
+* Si ambos entrenadores sacan a batalla el mismo pokemon, no podrán pelear.
+* Se va narrando en texto la batalla.
+
+##Técnico:
+* Los entreadores y pokemones son objetos JS.
+* Estilos CSS en el HTML.
+* Uso de Header y Footer HTML5.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,59 @@
-main();
+const Pikachu = new Pokemon("Pikachu", "Amarillo", 100);
+const Charmander = new Pokemon("Charmander","Rojo",20);
+const Squirtle = new Pokemon("Squirtle", "Azul", 50);
+const Bulbasaur = new Pokemon("Bulbasaur","Verde",55);
+const Butterfly = new Pokemon("Butterfly", "Lila", 25);
+const Mew = new Pokemon("Mew", "Blanco", 100);
+const Jiglypuff = new Pokemon("Jiglypuff", "Rosa", 30);
+const Staryu = new Pokemon("Staryu", "Cafe", 80);
+const Oddish = new Pokemon("Oddish", "Uva", 40);
+const Onix = new Pokemon("Onix", "Gris", 90);
 
-function main() {
-    const Pikachu = new Pokemon("Pikachu", "Amarillo", 100);
-    const Charmander = new Pokemon("Charmander","Rojo",20);
-    const Squirtle = new Pokemon("Squirtle", "Azul", 50);
-    const Bulbasaur = new Pokemon("Bulbasaur","Verde",55);
-    const Butterfly = new Pokemon("Butterfly", "Lila", 25);
-    const Mew = new Pokemon("Mew", "Blanco", 100);
-    const Jiglypuff = new Pokemon("Jiglypuff", "Rosa", 30);
-    const Staryu = new Pokemon("Staryu", "Cafe", 80);
-    const Oddish = new Pokemon("Oddish", "Uva", 40);
-    const Onix = new Pokemon("Onix", "Gris", 90);
+const Ash = new Entrenador("Ash", [Pikachu,Charmander,Squirtle,Bulbasaur,Butterfly,Mew,Jiglypuff,Staryu,Oddish,Onix], "pokemon1");
+Ash.listarPokemones();
 
-    var pokemones = [Pikachu,Charmander,Squirtle,Bulbasaur,Butterfly,Mew,Jiglypuff,Staryu,Oddish,Onix];
+const Brock = new Entrenador("Brock", [Pikachu,Charmander,Squirtle,Bulbasaur,Butterfly,Mew,Jiglypuff,Staryu,Oddish,Onix], "pokemon2");
+Brock.listarPokemones();
 
-    rellenarSelectConPokemones("pokemon1", pokemones);
-    rellenarSelectConPokemones("pokemon2", pokemones);
+
+function ordenarAtaque(Entrenador) {
+    var nombrePokemonElegido1 = document.getElementById("pokemon1").value;
+    var nombrePokemonElegido2 = document.getElementById("pokemon2").value;
+    var narracion = document.getElementById("narracion");
+
+    pokemonElegido1 = Ash.pokemones.filter(function(pokemon){
+        return pokemon.nombre == nombrePokemonElegido1;
+    })[0];
+    pokemonElegido2 = Brock.pokemones.filter(function(pokemon){
+        return pokemon.nombre == nombrePokemonElegido2;
+    })[0];
+    console.log(pokemonElegido1);
+    if(pokemonElegido1 == undefined || pokemonElegido2 == undefined){
+        narracion.innerHTML = "Ambos entrenadores deben elegir un pokemon.";
+    } else if(pokemonElegido1 === pokemonElegido2){
+        narracion.innerHTML = "¡No pueden elegir el mismo pokemon!. Elijan dos pokemones diferentes.";
+    } else{
+        if(Entrenador == "Ash"){
+            if(pokemonElegido1.vida <= 0){
+                narracion.innerHTML = "Tu pokemon " + pokemonElegido1.nombre + " no tiene energía para pelear. Debes elegir otro pokemon.";
+            } else if(pokemonElegido2.vida <= 0){
+                narracion.innerHTML = pokemonElegido2.nombre + " no se puede atacar, ya está muy débil. Debes pelear contra otro pokemon.";
+            }else{
+                pokemonElegido1.atacar(pokemonElegido2);
+                narracion.innerHTML = pokemonElegido1.nombre + " atacó a " + pokemonElegido2.nombre + " y " + pokemonElegido2.nombre + " tiene una vida de " + pokemonElegido2.vida;
+            }  
+        } else if(Entrenador == "Brock"){
+            if(pokemonElegido2.vida <= 0){
+                narracion.innerHTML = "Tu pokemon " + pokemonElegido2.nombre + " no tiene energía para pelear. Debes elegir otro pokemon.";
+            } else if(pokemonElegido1.vida <= 0){
+                narracion.innerHTML = pokemonElegido1.nombre + " no se puede atacar, ya está muy débil. Debes pelear contra otro pokemon.";
+            }else{
+                pokemonElegido2.atacar(pokemonElegido1);
+                narracion.innerHTML = pokemonElegido2.nombre + " atacó a " + pokemonElegido1.nombre + " y " + pokemonElegido1.nombre + " tiene una vida de " + pokemonElegido1.vida;
+            }  
+        }
+
+    } 
 }
 
 function Pokemon(nombre,color, poderDeAtaque){
@@ -38,17 +76,19 @@ function Pokemon(nombre,color, poderDeAtaque){
     }
 }
 
-function rellenarSelectConPokemones(id, pokemones) {
-    var selectPokemon = document.getElementById(id);
+function Entrenador(nombre, pokemones, id) {
+    this.nombre = nombre;
+    this.pokemones = pokemones;
+    this.id = id;
 
-    for(i=0; i<pokemones.length; i++){
-        var option = document.createElement("option");
-        option.text = pokemones[i].nombre;
-        option.value = pokemones[i].nombre;
-        selectPokemon.add(option);
+    this.listarPokemones = function(){
+        var selectPokemon = document.getElementById(this.id);
+
+        for(i=0; i<this.pokemones.length; i++){
+            var option = document.createElement("option");
+            option.text = this.pokemones[i].nombre;
+            option.value = this.pokemones[i].nombre;
+            selectPokemon.add(option);
+        }
     }
 }
-
-//Pikachu.atacar(Charmander);
-
-//console.log(Charmander.vida);
